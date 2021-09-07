29fc9c0668ab9ee1646089f0d04151d392d2c469
RobiulMan <robiulislam.bg@gmail.com>
2021-09-08 02:28:31 +0600

    static data with modal component
    set static data for every item also created modal to show description into the every item

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar.jsx";
+import HomePage from "./screen/HomePage.jsx";
 function App() {
   return (
     <div>
       <Navbar />
+      <HomePage/>
     </div>
   );
 }
