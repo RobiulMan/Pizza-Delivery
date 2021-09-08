32d3afda8887cee1cf167730be70a607a877dd61
RobiulMan <robiulislam.bg@gmail.com>
2021-09-09 02:04:39 +0600

    MongoDB Model Design
    pizza schema designed with insert static data

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const dbConnection = require('./DB/dbConnection')
-dbConnection()
-const PORT = 3000 || process.env.PORT
+const Pizza = require('./models/Pizza');
+
+const app = express();
+
+const dbConnection = require('./DB/dbConnection');
+
+dbConnection();
+const PORT = 3000 || process.env.PORT;
 app.use(express.json());
-app.use(cors())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.status(200).send({hello: 'word'}))
+app.get('/api/getpizza', async (req, res) => {
+    try {
+        const data = await Pizza.find({});
+        res.status(200).json(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
+app.get('/', (req, res) => res.status(200).send({ hello: 'word' }));
 
-
-
-app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'));
