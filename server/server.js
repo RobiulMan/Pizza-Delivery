18a4836900e76c83d54707f393066acb9f8b76d8
Robul Islam <49164744+RobiulMan@users.noreply.github.com>
2022-08-04 20:06:39 +0600

    fixed the path #7

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static('../client/build'));
     app.get('*', function (req, res) {
        // const index = path.join(__dirname, 'build', 'index.html');
-       const index = path.resolve('../client/build/', 'index.html')
+       const index = path.resolve('/app/client/build/', 'index.html')
         res.sendFile(index);
     });
 }
